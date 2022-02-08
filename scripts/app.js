d16cbe4662171fd54bece3d4c705fca5084c15cb
Tyson Gomes <tkg@TKG-MacBook-Pro.local>
2022-02-08 15:18:08 -0800

    Added and filled an array of objects for pets

---
+++ scripts/app.js
@@ -0,0 +1,86 @@
+/* === Variables === */
+
+const pets =
+  [
+    {
+      name: "Bulbasaur",
+      pokedex: 1,
+      age: 1,
+      hungry: 1,
+      sleepy: 1,
+      bored: 1,
+      image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png"
+    },
+    {
+      name: "Ivysaur",
+      pokedex: 2,
+      age: 1,
+      hungry: 1,
+      sleepy: 1,
+      bored: 1,
+      image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/002.png"
+    },
+    {
+      name: "Venusaur",
+      pokedex: 3,
+      age: 1,
+      hungry: 1,
+      sleepy: 1,
+      bored: 1,
+      image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/003.png"
+    },
+    {
+      name: "Charmander",
+      pokedex: 4,
+      age: 1,
+      hungry: 1,
+      sleepy: 1,
+      bored: 1,
+      image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png"
+    },
+    {
+      name: "Charmeleon",
+      pokedex: 5,
+      age: 1,
+      hungry: 1,
+      sleepy: 1,
+      bored: 1,
+      image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/005.png"
+    },
+    {
+      name: "Charizard",
+      pokedex: 6,
+      age: 1,
+      hungry: 1,
+      sleepy: 1,
+      bored: 1,
+      image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/006.png"
+    },
+    {
+      name: "Squirtle",
+      pokedex: 7,
+      age: 1,
+      hungry: 1,
+      sleepy: 1,
+      bored: 1,
+      image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/007.png"
+    },
+    {
+      name: "Wartortle",
+      pokedex: 8,
+      age: 1,
+      hungry: 1,
+      sleepy: 1,
+      bored: 1,
+      image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/008.png"
+    },
+    {
+      name: "Blastoise",
+      pokedex: 9,
+      age: 1,
+      hungry: 1,
+      sleepy: 1,
+      bored: 1,
+      image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/009.png"
+    },
+  ];
