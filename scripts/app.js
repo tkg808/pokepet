a15ffb268c767db3877b097fa5ec45b939513d35
Tyson Gomes <tkg@TKG-MacBook-Pro.local>
2022-02-09 17:03:13 -0800

    Added event listeners for interacting with the pet and updating the display

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,9 @@
 /* === Variables === */
 
+const logo = "http://4.bp.blogspot.com/-oH465XaJjpY/T8yZVJsteTI/AAAAAAAAADs/LwFrlgaVVv0/s1600/dog_paw_print_black.png"
+
+const egg = "https://www.pinclipart.com/picdir/big/89-892298_pokemon-egg-pokemon-go-egg-png-clipart.png"
+
 const pets =
   [
     {
@@ -85,21 +89,120 @@ const pets =
     },
   ];
 
+let currentPet =
+{
+  name: "Charmander",
+  pokedex: 0,
+  age: 1,
+  hungry: 2,
+  sleepy: 2,
+  bored: 2,
+  image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png"
+};
+
+let isAlive = true;
+
+// const intervalID = setInterval(agePet, 1000);
+
 /* === DOM Elements === */
 
 const $image = $(".pet__image");
 const $feed = $(".interact__feed");
 const $sleep = $(".interact__sleep");
 const $play = $(".interact__play");
+const $pause = $(".button__pause");
+
+$image.attr("src", logo);
 
 /* === Functions === */
 
+function agePet()
+{
+  currentPet.age += 1;
 
+  $(".info__age").text(`Age: ${currentPet.age}`);
+}
 
 /* === Event Functions === */
 
+const checkState = function ()
+{
+  if ($image.attr("src") === logo)
+  {
+    $image.attr("src", currentPet.image);
+    $(".info__name").text(`Name: ${currentPet.name}`);
+    $(".info__age").text(`Age: ${currentPet.age}`)
+    $(".stat__hungry").text(currentPet.hungry);
+    $(".stat__sleepy").text(currentPet.sleepy);
+    $(".stat__bored").text(currentPet.bored);
+  }
+  else
+  {
+    $image.attr("src", logo);
+  }
+}
+
+const feedPet = function ()
+{
+  if ($image.attr("src") !== logo)
+  {
+    if (currentPet.hungry === 1)
+    {
+      alert(`${currentPet.name} is not hungry!`)
+    }
+    else
+    {
+      currentPet.hungry -= 1;
+    }
+  }
+
+  $(".stat__hungry").text(currentPet.hungry)
+}
+
+const sleepPet = function ()
+{
+  if ($image.attr("src") !== logo)
+  {
+    if (currentPet.sleepy === 1)
+    {
+      alert(`${currentPet.name} doesn't want to nap!`);
+    }
+    else
+    {
+      currentPet.sleepy -= 1;
+    }
+  }
+
+  $(".stat__sleepy").text(currentPet.sleepy);
+}
+
+const playPet = function ()
+{
+  if ($image.attr("src") !== logo)
+  {
+    if (currentPet.bored === 1)
+    {
+      alert(`${currentPet.name} is done playing!`);
+    }
+    else
+    {
+      currentPet.bored -= 1;
+    }
+  }
+
+  $(".stat__bored").text(currentPet.bored);
+}
+
 /* === Event Listeners === */
 
+$pause.on("click", checkState)
 
+$feed.on("click", feedPet);
+
+$sleep.on("click", sleepPet);
+
+$play.on("click", playPet);
 
 /* === Invoked Functions === */
+
+console.log($image.attr("src") === "");
